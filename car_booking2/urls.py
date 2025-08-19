07398902263eca80539d5979fd2762cd3dc96a20
unknown <pankajmj123@gmail.com>
2025-08-19 21:43:14 +0530

    Fixed identation error

--- car_booking2/urls.py
+++ car_booking2/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('myapp.url'))
+    path('',include('myapp.url')),
+    path('login/', views.index, name='login'),
+    path('signup/', views.index, name='register'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from django.contrib.auth.models import User    
+from django.contrib.auth import authenticate, login   
+from django.contrib import messages  
+
 
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email= request.POST.get('email')
+        User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+        return redirect('/')  
+
     return render(request,'idex.html')
 
 def about(request):
@@ -19,4 +34,21 @@ def gallery(request):
 
 def booking(request):
     return render(request,'booking.html')
+
+
+
+def signup_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+        else:
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+            login(request, user)   
+            return redirect("/")   
+
+    return render(request, "signup.html")   
 
